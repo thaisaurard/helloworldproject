a62cef627e6a47c70e2296046f8d1d05047fe994
Thais Aurard <thais.aurard@etu.umontpellier.fr>
2017-05-25 16:56:53 +0200

    Implémentation de l'ajout de personnage (fichiers manquants)

---
+++ Ajout_Personnage.php
@@ -0,0 +1,1 @@
+<?php require_once("view/Ajout_perso.php"); ?>
---
+++ controller/Controller_Ajout_Perso.php
@@ -0,0 +1,25 @@
+<?php
+	require_once ('../model/User.php');
+  require_once ('../model/Character.php');
+	ini_set('display_errors',1);
+
+	$CharacterName = htmlspecialchars($_POST['CharacterName']);
+	$CharacterInfos = htmlspecialchars($_POST['CharacterInfos']);
+  $CharacterAge = htmlspecialchars($_POST['CharacterAge']);
+
+  $idRace = 0;
+
+
+  $idUser = User::Get_user_Id($_COOKIE["codeconnexion"]);
+
+  if (empty($CharacterName || empty($CharacterInfos)) ) {
+		$messageErreur = "Merci de renseigner tous les champs! ";
+    header("Location: ../Erreur.php?erreur=".$messageErreur);
+  }
+  else
+  {
+    Character::add_character($CharacterName,$CharacterInfos,$idUser,$idRace);
+  }
+
+  header("Location: ../Personnages.php");
+?>
