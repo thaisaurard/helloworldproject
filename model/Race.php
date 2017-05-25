2cf7b6d426d8b7d88ec8b736343118346ce111a9
Thais Aurard <thais.aurard@etu.umontpellier.fr>
2017-05-25 21:24:27 +0200

    CA MARCHE BORDEL DE CUL

--- model/Race.php
+++ model/Race.php
@@ -31,5 +31,38 @@
 			$req->bindParam(':iduser',$user);
 			$req->execute();
 		}
+
+		public static function dropdown_race(){
+			require_once('Pdo.php');
+			require_once ('model/User.php');
+
+			$idUser = User::Get_user_Id($_COOKIE["codeconnexion"]);
+
+			$bdd=connexion();
+			$reponse = $bdd->query('SELECT * FROM "Race"');
+			$printed_text = "";
+
+			// print_r($reponse);
+			// On affiche chaque entrée une à une
+			while ($donnees = $reponse->fetch())
+			{
+				if ($idUser == $donnees['iduser']){
+					$item = "<option value=".$donnees['idrace'].">".$donnees['RaceName']."</option>";
+					$printed_text = $printed_text.$item;
+				}
+			}
+			return $printed_text;
+		}
+
+		public static function get_race_name($id){
+			require_once('Pdo.php');
+			$bdd=connexion();
+			
+			$reponse = $bdd->query('SELECT * FROM "Race" WHERE "idrace" = '.$id);
+			while ($donnees = $reponse->fetch()){
+				return $donnees['RaceName'];
+			}
+			return "Race inconnue";
+		}
 	}
 ?>
