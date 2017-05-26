b57c3437f99b1cb775aa3df9f9ac23544e3584a7
thaisaurard <thais.aurard@etu.umontpellier.fr>
2017-05-26 19:34:27 +0200

    commit

--- accueil.php
+++ accueil.php
@@ -6,8 +6,14 @@
         <link rel="stylesheet" href="css/materialize/css/materialize.css">
     </head>
     <body>
+      <div>
         <?php require ("view/header.php");?>
+      </div>
+      <div>
         <h1 style="text-align:center">Hello World</h1>
+      </div>
+      <div>
         <h5 style="text-align:center"> Créez et gérez des univers fictifs <h5>
+      </div>
     </body>
 </html>
